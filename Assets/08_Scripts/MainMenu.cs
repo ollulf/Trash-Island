dbef09f3317fd5887e41cb07a3126b2a831a2d96
Alex <alexander.v.anhalt@gmail.com>
2020-07-14 13:24:29 +0200

    Main Menu

---
+++ Assets/08_Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void OnNextClick()
+    {
+        Application.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+
+}
