a42eb44bdbc433267fd2a11ab5a3b2651b2d3c3a
Jean.Isserstedt <jean.isserstedt@gmx.de>
2020-07-14 20:01:17 +0200

    added posessable Boat and jean test scene

---
+++ Assets/08_Scripts/BoatController.cs
@@ -0,0 +1,80 @@
+﻿using Ditzelgames;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    public Transform Motor;
+    public float SteerPower = 5f;
+    public float Power = 5f;
+    public float MaxSpeed = 10f;
+    public float Drag = 0.1f;
+    float Rotation = 0;
+    public float MaxAngularVel = 1f;
+    public AnimationCurve DotToDrag;
+
+    protected Rigidbody Rigidbody;
+    protected Quaternion StartRotation;
+
+    public void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        StartRotation = Motor.localRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 forceDirection = transform.forward;
+
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rotation = Mathf.Lerp(Rotation, 60, Time.fixedDeltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rotation = Mathf.Lerp(Rotation, -60, Time.fixedDeltaTime);
+        }
+        else
+        {
+            Rotation = Mathf.Lerp(Rotation, 0, Time.fixedDeltaTime);
+        }
+
+        // Rotation = Mathf.Clamp(Rotation, -60, 60);
+        Motor.localEulerAngles = new Vector3(0, Rotation, 0);
+        Vector3 force = Motor.forward * Power;
+        Rigidbody.maxAngularVelocity = MaxAngularVel;
+        if (Input.GetKey(KeyCode.W))
+        {
+            Rigidbody.AddForceAtPosition(force, Motor.position);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Rigidbody.AddForceAtPosition(-force, Motor.position);
+        }
+
+        float dot = Vector3.Dot(Rigidbody.velocity.normalized, transform.forward);
+
+        // Rigidbody.velocity = Rigidbody.velocity * (1-(DotToDrag.Evaluate(dot)*Drag));
+
+        if (Rigidbody.velocity.magnitude >= MaxSpeed)
+        {
+            Rigidbody.velocity = Rigidbody.velocity.normalized * MaxSpeed;
+        }
+        if(dot > 0)
+        {
+           
+            Rigidbody.velocity = Vector3.Lerp(Rigidbody.velocity, transform.forward * Rigidbody.velocity.magnitude, DotToDrag.Evaluate(Mathf.Abs(dot)));
+        }
+        else
+        {
+            Rigidbody.velocity = Vector3.Lerp(Rigidbody.velocity, transform.forward  * -Rigidbody.velocity.magnitude, DotToDrag.Evaluate(Mathf.Abs(dot)));
+            
+        }
+
+    }
+
+
+
+}
---
+++ Assets/08_Scripts/PossessBoat.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossessBoat : MonoBehaviour
+{
+    public GameObject boat;
+    public GameObject player;
+    public GameObject boatCamera;
+
+    bool IsPossessed = false;
+    bool InTrigger = false;
+    public void Start()
+    {
+        UnPossess();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InTrigger = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) &&InTrigger == true)
+        {
+            Possess();
+        }
+        if (Input.GetKeyDown(KeyCode.F) && InTrigger == false)
+        {
+            UnPossess();
+        }
+    }
+
+    private void Possess()
+    {
+
+        Debug.Log("E was pressed");
+
+
+        player.GetComponent<PlayerMovement>().enabled = false;
+        boat.GetComponent<BoatController>().enabled = true;
+        player.GetComponentInChildren<Camera>().enabled = false;
+        boatCamera.SetActive(true);
+        IsPossessed = true;
+        player.transform.parent = boat.transform;
+    }
+
+    private void UnPossess()
+    {
+        player.GetComponent<PlayerMovement>().enabled = true;
+        boat.GetComponent<BoatController>().enabled = false;
+        player.GetComponentInChildren<Camera>().enabled = true;
+        boatCamera.SetActive(false);
+        IsPossessed = false;
+        player.transform.parent = null;
+    }
+
+}
