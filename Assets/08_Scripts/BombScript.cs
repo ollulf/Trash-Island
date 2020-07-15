bd78c3313d67145baab2289046a16380ee213a81
ollulf <max.bredlau@googlemail.com>
2020-07-15 14:58:35 +0200

    Added Bombs "explode"

---
+++ Assets/08_Scripts/BombScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject trashParticles;
+
+    public bool isActive = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (isActive)
+        {
+            ExplosionVisualisation();
+
+            Debug.Log("Boom!");
+
+            Destroy(gameObject);
+        }
+        
+        
+    }
+
+    private void ExplosionVisualisation() 
+    {
+        //VIsualisation
+    }
+}
