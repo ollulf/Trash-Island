e4eeb6352b57e0c2060135dff8d874688cc1143a
ollulf <max.bredlau@googlemail.com>
2020-07-16 14:03:42 +0200

    Added Explosion to bombs  and added destroyable pipes

---
+++ Assets/08_Scripts/DestroyablePipe.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyablePipe : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject thisPipe;
+
+    [SerializeField]
+    private GameObject destroyedPipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Bomb")
+        {
+            GameObject newPipe = Instantiate(destroyedPipe, thisPipe.transform.position, thisPipe.transform.rotation) as GameObject;
+            newPipe.transform.localScale = thisPipe.transform.localScale;
+
+            Destroy(thisPipe);
+        }
+    }
+}
