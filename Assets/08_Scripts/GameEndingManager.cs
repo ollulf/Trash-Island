ef58ed07e2ac54c2e6d96843b09c13b6b4ebe151
ollulf <max.bredlau@googlemail.com>
2020-07-17 12:46:05 +0200

    Worked on game endging script

---
+++ Assets/08_Scripts/GameEndingManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndingManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject[] pipesToDestroy;
+
+
+    void Update()
+    {
+        Debug.Log(pipesToDestroy.Length);
+
+        if (CheckIfAllDestroyed())
+        {
+            SceneManager.LoadScene(6);
+        }
+    }
+
+    private bool CheckIfAllDestroyed()
+    {
+        if (pipesToDestroy [0] == null)
+            return true;
+
+        else
+            return false;
+    }
+}
